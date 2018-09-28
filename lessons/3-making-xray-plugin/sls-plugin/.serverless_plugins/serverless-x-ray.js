fa48a1c27338ff31163dbab0aeeb674bc8cb9600
Ryota Hirano <hirano@tsumikiinc.com>
2018-09-28 17:29:06 +0900

    finish lesson3

--- lessons/3-making-xray-plugin/sls-plugin/.serverless_plugins/serverless-x-ray.js
+++ lessons/3-making-xray-plugin/sls-plugin/.serverless_plugins/serverless-x-ray.js
@@ -4,9 +4,42 @@ class ServerlessXRay {
   constructor(serverless, options) {
     this.serverless = serverless;
     this.options = options;
-    this.commands = {};
-    this.hooks = {};
+    this.provider = this.serverless.getProvider('aws');
 
+    this.xRayConfig = {
+      "Effect": "Allow",
+      "Action": [
+        "xray:PutTraceSegments",
+        "xray:PutTelemetryRecords"
+      ],
+      "Resource": "*"
+    }
+
+    this.commands = {
+      //
+    };
+
+    this.hooks = {
+      'after:package:compileFunctions': this.Trace.bind(this)
+    };
+  }
+
+  Trace() {
+    const custom = this.serverless.service.custom;
+
+    if (custom.trace !== true) {
+      return;
+    } else {
+      this.serverless.service.getAllFunctions().forEach((functionName) => {
+        const functionLogicalId = this.provider.naming.getLambdaLogicalId(functionName);
+        this.serverless.service.provider.compiledCloudFormationTemplate
+          .Resources[functionLogicalId].Properties.TracingConfig = {
+          'Mode': 'Active'
+        };
+      });
+      this.serverless.service.provider.compiledCloudFormationTemplate.Resources
+        .IamRoleLambdaExecution.Properties.Policies[0].PolicyDocument.Statement.push(this.xRayConfig);
+    }
   }
 }
 
