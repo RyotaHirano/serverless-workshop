628f07dbd0f9eb4377c583d8083ddfe6728aec32
Ryota Hirano <hirano@tsumikiinc.com>
2018-09-28 15:45:19 +0900

    finish lesson2

--- lessons/2-making-new-command-plugin/sls-plugin/.serverless_plugins/serverless-download.js
+++ lessons/2-making-new-command-plugin/sls-plugin/.serverless_plugins/serverless-download.js
@@ -4,12 +4,49 @@ class ServerlessDownload {
   constructor(serverless, options) {
     this.serverless = serverless;
     this.options = options;
+    this.provider = this.serverless.getProvider('aws');
+    this.region = this.provider.getRegion();
+    this.stage = this.provider.getStage();
 
     this.commands = {
+      'download': {
+        usage: 'Download DynamoDB Serverless plugin',
+        lifecycleEvents: [
+          'downloadData'
+        ],
+        options: {
+          resource: {
+            usage: 'Specify name of resource for your table',
+            required: true
+          },
+          'target-stage': {
+            usage: 'Stage you want to upload data to',
+            required: true
+          }
+        }
+      }
     };
 
     this.hooks = {
+      'download:downloadData': this.downloadData.bind(this)
     };
+  }
+
+  downloadData() {
+    const tableName = this.serverless.service.resources.Resources[this.options.resource].Properties.TableName;
+
+    return this.provider.request('DynamoDB',
+      'scan',
+      { TableName: tableName },
+      this.options.stage,
+      this.options.region
+    ).then(result => {
+      const items = result.Items;
+      items.map(item => {
+        this.serverless.cli.log(`e-mail: ${item.EmailAddress.S}`);
+        this.serverless.cli.log(`name: ${item.Name.S}`);
+      });
+    })
   }
 }
 
